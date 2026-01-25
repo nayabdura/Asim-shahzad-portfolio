bbd5ea3d04d8ce6bbc15f21c4534782068ea1232
Abdullah Imran <119076137+nayabdura@users.noreply.github.com>
2026-01-25 02:48:47 -0800

    use client added

--- app/who-am-i/page.tsx
+++ app/who-am-i/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link"; // For better navigation performance
 import { ArrowUpRight, Monitor, Smartphone, Image as ImageIcon } from "lucide-react";
